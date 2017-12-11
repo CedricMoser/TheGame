e586ce50975c4f81f56c2e6ad9c3389425331509
nioxx <eppletobias@web.de>
2017-12-11 16:06:51 +0100

    GuiEvents implented (not ready yet)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-import fnt.FNTDocument;
-import fnt.FNTParser;
 import graphics.Renderer2D;
-import graphics.Texture;
 import window.Window;
 
-import java.io.*;
 
-import static org.lwjgl.opengl.GL11.GL_NO_ERROR;
-import static org.lwjgl.opengl.GL11.glGetError;
 
 public class Main {
     public static void main(String[] args) {
@@ -39,8 +33,8 @@ public class Main {
         long begin = System.currentTimeMillis();
         int  fps = 0;
 
-        Texture tex = new Texture("test.png");
-        Texture tex2 = new Texture("test2.png");
+        //Texture tex = new Texture("test.png");
+        //Texture tex2 = new Texture("test2.png");
 
         while (window.isOpen()) {
             window.clear();
@@ -56,8 +50,8 @@ public class Main {
             renderer.setColor(0x0000FFFF);
             renderer.drawRect(230.0f, 10.0f, 100.0f, 25.0f, Renderer2D.RECT_CUT_LEFT_TOP | Renderer2D.RECT_CUT_RIGHT_BOTTOM);
 
-            renderer.drawImage(10.0f, 150.0f, tex.getWidth(), tex.getHeight(), tex);
-            renderer.drawImage(10.0f, 300.0f, tex2.getWidth(), tex2.getHeight(), tex2);
+            // renderer.drawImage(10.0f, 150.0f, tex.getWidth(), tex.getHeight(), tex);
+            // renderer.drawImage(10.0f, 300.0f, tex2.getWidth(), tex2.getHeight(), tex2);
 
             renderer.drawString(15.0f, 15.0f, "Hello world");
 
